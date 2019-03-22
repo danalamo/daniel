15bd1a2094a045c40cd98490c461a388880d9792
Daniel Alamo <daniel.alamo@gmail.com>
2019-03-22 11:08:52 -0400

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+        <meta property="og:url"                content="https://danielalamo.com" />
+        <meta property="og:type"               content="article" />
+        <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
+        <meta property="og:description"        content="How much does culture influence creative thinking?" />
+        <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
