2c58c0e002f76381a954f239fd088b72ce3b385e
Daniel Alamo <dalamo@shepardes.com>
2019-04-13 18:54:22 -0400

    Add `/verse` endpoint

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/welcome', 'welcome');
+Route::view('/', 'daniel');
+Route::get('/verse', function()
+{
+    $data = file_get_contents("http://www.ourmanna.com/verses/api/get/?format=json&order=random");
+    $res = json_decode($data);
+    /*  
+        verse: 
+            details:
+                reference: "Matthew 5:6"
+                text: "Blessed are those who hunger and thirst for righteousness, for they will be filled."
+                verseurl: "http://www.ourmanna.com/"
+                version: "NIV
+    */
+    $verse = $res->verse->details;
+    
+    $response = [
+        'text' => $verse->text, 
+        'reference' => $verse->reference, 
+        'version' => $verse->version, 
+    ];
+    
+    return response($response);
 });
