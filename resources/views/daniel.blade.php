6d910c0d3d04597586e004cc1120ffce72b38119
Daniel Alamo <dalamo@shepardes.com>
2019-04-13 18:38:53 -0400

    Add `daniel.blade.php`

---
+++ resources/views/daniel.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta property="og:url"                content="https://danielalamo.com" />
+        <meta property="og:type"               content="article" />
+        <meta property="og:title"              content="Support NMCYA Team - June Blue Skies Mission Trip" />
+        <meta property="og:description"        content="Partner with Blue Skies Ministries to provide a weeklong beach retreat for families battling pediatric cancer. Let's afford them the opportunity to relax, play, build community, have fun and above all take a break from the daily struggle of living with pediatric cancer." />
+        <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="d-logo.png?v=1.1">
+        {{--<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.0/semantic.min.css">--}}
+        <title>Daniel Alamo</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- Styles -->
+        <style type="text/css">
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+                font-size: 16px;
+                font-size: 11px !important;
+            }
+            
+            body {
+                background: url(background-laravel-beach.jpeg);
+                background-size: cover;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-rel {
+                position: relative;
+            }
+
+            @media screen and (max-width: 1200px) {
+                html, body {                
+                    font-size: 14px;
+                }
+            }
+
+            @media screen and (max-width: 500px) {
+                html, body {                
+                    font-size: 12px;
+                }
+            }
+        </style>
+        <script src="https://use.fontawesome.com/94dd692c46.js"></script>
+    </head>
+    <body>
+        <div 
+            id="app" 
+            class="flex-center position-rel full-height">
+
+            <v-daniel></v-daniel>
+            
+        </div>
+        
+        <script src="/js/manifest.js"></script>
+        <script src="/js/vendor.js"></script>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
