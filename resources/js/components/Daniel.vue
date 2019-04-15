cb785eadc419d32b7b89c9565e9898e226d92774
Daniel Alamo <dalamo@shepardes.com>
2019-04-15 06:32:39 -0400

    Update `Daniel.vue`

--- resources/js/components/Daniel.vue
+++ resources/js/components/Daniel.vue
@@ -5,7 +5,7 @@
         top: 18px;
     }
 
-    .content {
+    .mcontent {
         text-align: center;
         #background: #fffffff7;
         background: rgba(255, 255, 255, 0.94);
@@ -41,8 +41,18 @@
     }
     .links > a:hover {
         transform: scale(1.4);
-
         /*transition-delay: 1.5;*/
+    }
+    
+    .raise {
+        transition: .5s !important;
+    }
+    .raise:hover {
+        transform: scale(1.2);
+    }
+    
+    .ui.header {
+        color: #636b6f;
     }
 
     .m-b-md {
@@ -57,60 +67,83 @@
 </style>
 
 <template>
-    <Motion 
-        :values="$data" 
-        tag="div" 
-        :spring="{ stiffness: 110, damping: 10 }"
-    >
-        <div 
-            class="content" 
-            slot-scope="obj" 
-            :style="{ 
-                transform: `
-                    translateY(${obj.offset}px) 
-                    scale(${obj.scale})
-                `,
-            }"
-            
-            _touchstart="track"
-            _touchmove="move"
-
-            _mousedown="track"
-            _mouseup="stop"
-            _mouseleave="stop"
-            _mousemove="move"
+    <div>
+        <Motion 
+            :values="$data" 
+            tag="div" 
+            :spring="{ stiffness: 110, damping: 10 }"
         >
-            <img class="mypic"
-                 src="https://avatars1.githubusercontent.com/u/6430396?v=4&s=460" />
+            <div 
+                class="mcontent" 
+                slot-scope="obj"
+                
+                style="{ 
+                    transform: `
+                        translateY(${obj.offset}px) 
+                        scale(${obj.scale})
+                    `,
+                }"
+                
+                _touchstart="track"
+                _touchmove="move"
     
-            <h1>Daniel Alamo</h1>
-            <div class="title m-b-md">
-                software engineer
+                _mousedown="track"
+                _mouseup="stop"
+                _mouseleave="stop"
+                _mousemove="move"
+            >
+                <img class="mypic"
+                     src="https://avatars1.githubusercontent.com/u/6430396?v=4&s=460" />
+        
+                <h1>Daniel Alamo</h1>
+                <div class="title m-b-md">
+                    software engineer
+                </div>
+                
+                <div class="links">
+                    <h2>🖥 + ☕️ + 🎶 = ❤️</h2>
+                    <a href="//linkedin.com/in/dalamo">
+                        <i class="round fa fa-linkedin"></i>
+                    </a>
+                    <a href="//github.com/danalamo">
+                        <i class="round fa fa-github"></i>
+                    </a>
+                    <a href="//bitbucket.org/danalamo">
+                        <i class="round fa fa-bitbucket"></i>
+                    </a>
+                    
+                    <br><br>
+                    
+                    <a href="//laravel.com">Laravel</a>
+                    <a href="//laravel-news.com">News</a>
+                    
+                    <br>
+
+                    <h2 class="ui header" style="display:inline-block;" v-show="verse.text">
+                        <div class="sub header" style="max-width:280px;margin-bottom:10px;">
+                            {{ verse.text }}
+                        </div>
+                        <i class="fa fa-lg fa-book" style="display:inline;"></i>
+                        <div class="content">
+                            {{ verse.reference }}
+                        </div>
+                    </h2>
+                    
+                    <br>
+                    
+                    <!--<button @click="getVerse">Load Verse</button>-->
+                    <sui-button class="raise" :loading="loadingVerse" color="brown" content="Daily Bible Verse" @click="getVerse" />
+                    
+                    <!--<pre style="text-align:left;">{{ verses }}</pre>-->
+                    <!--<input type="text" v-model="verse.reference" />-->
+                    <!--<sui-button color="brown" content="Keep" @click="keepVerse" />-->
+                    <!--<button @click="keepVerse">keep</button>-->
+                </div>
             </div>
-            
-            <div class="links">
-                <h2>🖥 + ☕️ + 🎶 = ❤️</h2>
-                <a href="//linkedin.com/in/dalamo">
-                    <i class="round fa fa-linkedin"></i>
-                </a>
-                <a href="//github.com/danalamo">
-                    <i class="round fa fa-github"></i>
-                </a>
-                <a href="//bitbucket.org/danalamo">
-                    <i class="round fa fa-bitbucket"></i>
-                </a>
-                <br><br>
-                
-                <a href="//laravel.com">Laravel</a>
-                <a href="//laravel-news.com">News</a>
-                
-                <!--<button @click="getVerse">Load Verse</button>-->
-                
-                <!--<pre style="text-align:left;">{{ $data }}</pre>-->
-                
-            </div>
-        </div>
-    </Motion>
+        </Motion>
+
+        <span id="iframe"></span>
+    </div>
 </template>
 
 <script>
@@ -124,11 +157,16 @@
                 oPageY: null,
                 ePageY: null,
                 mouseY: null,
-                verse: null,
+                verse: {},
+                loadingVerse: false,
+                verses: []
             }
         },
         
         methods: {
+            keepVerse() {
+              this.verses.push(this.verse.reference)  
+            },
             track(e) {
                 const pageY = e instanceof TouchEvent ? e.touches[0].pageY : e.pageY;
                 
@@ -150,13 +188,29 @@
                 this.oPageY = null
             },
             async getVerse() {
+                this.loadingVerse = true
                 let res = await fetch(`/verse`);
                 this.verse = await res.json();
+                this.loadingVerse = false
+            },
+            setUserAgent(obj, userAgent) {
+                if (!obj.navigator) {
+                    obj.navigator = {userAgent:''}
+                }
+                if (obj.navigator.userAgent !== userAgent) {
+                    let userAgentProp = { get: function () { return userAgent; } };
+                    try {
+                        Object.defineProperty(obj.navigator, 'userAgent', userAgentProp);
+                    } catch (e) {
+                        obj.navigator = Object.create(window.navigator, {
+                            userAgent: userAgentProp
+                        });
+                    }
+                }
             }
         },
         
         mounted() {
-            console.log('Component mounted.')
             this.prev_offset = 0
             this.offset = 0
             this.scale = 1
